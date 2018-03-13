d70e895920acdf3243d11f7ea80aa96cb3998262
jltimm <jacobltimm@gmail.com>
2018-03-12 21:13:23 -0500

    Moved most of the dropdown stuff into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dropdown.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   debugPaintSizeEnabled = true;
   runApp(new MyApp());
 }
-
-final dropdownKey = new GlobalKey<DropdownWidgetState>();
 
 class MyApp extends StatelessWidget {
   @override
@@ -30,84 +29,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
-
-class DropdownWidget extends StatefulWidget {
-  DropdownWidget({ Key dropdownKey }) : super(key: dropdownKey);
-  DropdownWidgetState createState() => new DropdownWidgetState();
-}
-
-class DropdownWidgetState extends State<DropdownWidget> {
-  String _dropdown1Value;
-  String get dropdown1Value => _dropdown1Value;
-  String _dropdown2Value;
-  String get dropdown2Value => _dropdown2Value;
-
-  Widget simpleDropdown() {
-    return new ListTile(
-      title: const Text('Simple dropdown:'),
-      trailing: new DropdownButton<String>(
-        value: _dropdown1Value,
-        hint: const Text('Choose'),
-        onChanged: (String newValue) {
-          setState(() {
-            _dropdown1Value = newValue;
-          });
-        },
-        items: <String>['USD', 'GBP', 'Euro'].map((String value) {
-          return new DropdownMenuItem<String>(
-            value: value,
-            child: new Text(value),
-          );
-        }).toList(),
-      ),
-    );
-  }
-
-  Widget hintDropdown() {
-    return new ListTile(
-      title: const Text('Dropdown with a hint:'),
-      trailing: new DropdownButton<String>(
-        value: _dropdown2Value,
-        hint: const Text('Choose'),
-        onChanged: (String newValue) {
-          setState(() {
-            _dropdown2Value = newValue;
-          });
-        },
-        items: <String>['USD', 'GBP', 'Euro'].map((String value) {
-          return new DropdownMenuItem<String>(
-            value: value,
-            child: new Text(value),
-          );
-        }).toList(),
-      ),
-    );
-  }
-
-  Widget buildDropdownContainer() {
-    return new Container(
-      padding: const EdgeInsets.all(24.0),
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          simpleDropdown(),
-          const SizedBox(
-            height: 24.0,
-          ),
-          hintDropdown(),
-          const SizedBox(
-            height: 24.0,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget build(BuildContext context) {
-    return buildDropdownContainer();
-  }
-
 }
 
 class TextWidget extends StatefulWidget {
