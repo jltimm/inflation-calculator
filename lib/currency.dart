45d63a6591ff0b198592126077df796552803de9
jltimm <jacobltimm@gmail.com>
2018-03-18 18:39:11 -0500

    Moving dropdown out of its own class

--- lib/currency.dart
+++ lib/currency.dart
@@ -8,29 +8,52 @@ class CurrencyWidget extends StatefulWidget {
 }
 
 class CurrencyWidgetState extends State<CurrencyWidget> {
-  Widget build(BuildContext context) {
-    return new Column(
-      children: [
-        createCurrencyInput(widget.flag),
-        createYearInput(),
-      ]
+
+  String _dropdownValue;
+  String get dropdownValue => _dropdownValue;
+
+  Widget createDropdown(bool flag) {
+    return new DropdownButton<String>(
+      value: _dropdownValue,
+      hint: const Text('Choose'),
+      onChanged: (String newValue) {
+        setState(() {
+          _dropdownValue = newValue;
+        });
+      },
+      items: <String>['USD', 'GBP', 'Euro'].map((String value) {
+        return new DropdownMenuItem<String>(
+          value: value,
+          child: new Text(value),
+        );
+      }).toList(),
     );
   }
-}
 
-Widget createYearInput() {
-  return new TextField(
-    keyboardType: TextInputType.number,
-    decoration: new InputDecoration(labelText: "Enter year"),
-  );
-}
+  Widget createYearInput() {
+    return new TextField(
+      keyboardType: TextInputType.number,
+      decoration: new InputDecoration(labelText: "Enter year"),
+    );
+  }
 
-Widget createCurrencyInput(bool flag) {
+  Widget createCurrencyInput(bool flag) {
   //if (flag) {
     return new TextField(
       keyboardType: TextInputType.number,
       decoration: new InputDecoration(labelText: "Enter amount(\$)"),
     );
   //}
+  }
+
+  Widget build(BuildContext context) {
+    return new Column(
+      children: [
+        createCurrencyInput(widget.flag),
+        createYearInput(),
+        createDropdown(widget.flag)
+      ]
+    );
+  }
 }
 
